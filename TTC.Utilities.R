918399f605712cea0f8a7bff9a7cfeab8fb7f332
Durgesh-Kavishvar <durgesh.kavishvar@gmail.com>
2019-10-12 17:38:08 -0400

    updating the script with three functions to solve the probelsm

--- TTC.Utilities.R
+++ TTC.Utilities.R
@@ -1,1 +1,29 @@
 #TTC.Utilities.R
+
+my_data=read.csv("ttc-streetcar-delay-data-2014.csv")
+
+find.sum.delay=function(my_data) {
+
+  Incidents=unique(my_data$Incident)
+  mindelay=my_data[,7]
+
+  for (i in 1:length(Incidents)){
+    sumdelay[i]=sum(as.numeric(mindelay[my_data$Incident==Incidents[i]]),na.rm=TRUE)
+  }
+  return(sumdelay)
+}
+
+find.mean.mech=function(my_data){
+  Incidents=unique(my_data$Incident)
+  mindelay=my_data[,7]
+  new.incidents=as.character(Incidents)
+
+  meandelay= mean(as.numeric(mindelay[my_data$Incident==Incidents[which(new.incidents=="Mechanical")]]),na.rm=TRUE)
+  return(meandelay)
+}
+
+find.max.in.dec=function(my_data){
+  delay=delay[substr(dates,4,6)=="Dec"]
+  route=route[delay==max(delay[!is.na(delay)])]
+  return(route)
+}
