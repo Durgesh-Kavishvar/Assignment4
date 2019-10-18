7847107db790164ea9fe9659a9f4305b3de3bfd5
Durgesh-Kavishvar <durgesh.kavishvar@gmail.com>
2019-10-18 15:25:24 -0400

    Modified functions to give correct values

--- TTC.Utilities.R
+++ TTC.Utilities.R
@@ -2,28 +2,33 @@
 
 my_data=read.csv("ttc-streetcar-delay-data-2014.csv")
 
-find.sum.delay=function(my_data) {
+find.num.delay=function(my_data,incident) {
 
-  Incidents=unique(my_data$Incident)
+
   mindelay=my_data[,7]
+  numdelay=length(na.omit(mindelay[my_data$Incident==incident]))
 
-  for (i in 1:length(Incidents)){
-    sumdelay[i]=sum(as.numeric(mindelay[my_data$Incident==Incidents[i]]),na.rm=TRUE)
-  }
-  return(sumdelay)
+  #list_of_returns=list(numdelay,Incidents)
+  return(numdelay)
 }
 
-find.mean.mech=function(my_data){
+find.mean=function(my_data,incident){
   Incidents=unique(my_data$Incident)
   mindelay=my_data[,7]
   new.incidents=as.character(Incidents)
 
-  meandelay= mean(as.numeric(mindelay[my_data$Incident==Incidents[which(new.incidents=="Mechanical")]]),na.rm=TRUE)
+  meandelay= mean(as.numeric(mindelay[my_data$Incident==Incidents[which(new.incidents==incident)]]),na.rm=TRUE)
   return(meandelay)
 }
 
-find.max.in.dec=function(my_data){
-  delay=delay[substr(dates,4,6)=="Dec"]
-  route=route[delay==max(delay[!is.na(delay)])]
-  return(route)
+
+find.max.delay.route=function(my_data,month){
+  dates=my_data[,1];
+  route=my_data[,2];
+  min.delay=my_data[,7];
+  dec.route=route[substr(dates,4,6)==month];
+  dec.delay=min.delay[substr(dates,4,6)==month];
+  dec.data=data.frame("dec.route"=dec.route,"dec.delay"=dec.delay);
+  sum=xtabs(dec.delay~dec.route,dec.data)
+  return(sum)
 }
