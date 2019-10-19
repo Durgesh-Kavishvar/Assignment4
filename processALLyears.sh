587d9d1b155a3ef496a10f14a4f39323ec9153a1
Durgesh-Kavishvar <durgesh.kavishvar@gmail.com>
2019-10-18 23:41:45 -0400

    bash script to run all the .csv files in the directory

--- processALLyears.sh
+++ processALLyears.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #processALLyears
 
-for filename in *csv
+#test loop to find whether everything is getting printed...all the files that we are dealing with
+for filename in *csv #for loop over all the csv file in the given directory
 do
-    echo $filename
+    echo $filename #print all the files in the directory
 done
 
 for filename in *csv
 do
-    Rscript process.TTC.R $filename
+    Rscript process.TTC.R $filename #running R scipts for all the .csv files in the directory
 done
